b476a5cb426741f09657ec8881aaa4ae6b3bd3d6
Jonas Almeida <jalmeida@mathbiol.org>
2020-01-16 14:19:08 -0500

    weird stuff for Noor to figure out

--- noorquest.js
+++ noorquest.js
@@ -1,1 +1,11 @@
 console.log(`noorquest.js loaded at ${Date()}`)
+
+noorUI=(div="noorQuestDiv")=>{
+    if(typeof(div)=='string'){
+        div=document.getElementById(div)
+    }
+    console.log('some NickMarvelousThing happens in the right div',div)
+}
+
+
+noorUI()
